173243029c9bcb1769a55a1ac95bfb0d5f19a276
Yuddomack <darr880406@gmail.com>
2024-04-21 21:17:28 +0900

    cache handler

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+const nextConfig = {
+  cacheHandler: path.resolve("./cache-handler.js"),
+  cacheMaxMemorySize: 0,
+};
 
 export default nextConfig;
